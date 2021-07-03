45684df680eaae8c3c8948e58bbdf13c1850f542
fabricio <fabricio.jacob@outlook.com>
2021-07-02 21:56:08 -0300

    add

--- product-service/interfaceadapter/src/main/java/com/github/fabriciolfj/repository/ProductRepository.java
+++ product-service/interfaceadapter/src/main/java/com/github/fabriciolfj/repository/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.github.fabriciolfj.repository;
 
 import com.github.fabriciolfj.repository.model.ProductEntity;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>, Specification<ProductEntity> {
 }
