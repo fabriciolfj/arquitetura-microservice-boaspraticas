18e6fa2ce1508685444c60e8c8f2ff096c98e067
Fabricio <fabricio.jacob@outlook.com>
2021-08-30 20:37:46 -0300

    add

--- deploy-services.sh
+++ deploy-services.sh
@@ -2,6 +2,9 @@ kubectl apply -f ./kubernetes/namespace.yml
 
 kubectl config set-context $(kubectl config current-context) --namespace=microservices
 
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install my-release bitnami/kafka
+
 for f in kubernetes/helm/components/*; do helm dep up $f; done
 for f in kubernetes/helm/environments/*; do helm dep up $f; done
 helm dep ls kubernetes/helm/environments/dev-env/
