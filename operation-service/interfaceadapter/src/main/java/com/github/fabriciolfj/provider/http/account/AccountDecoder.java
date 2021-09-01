bc3d41a24d5746ab30262a27685c897b84fff1e8
Fabricio <fabricio.jacob@outlook.com>
2021-09-01 20:59:00 -0300

    dd

--- operation-service/interfaceadapter/src/main/java/com/github/fabriciolfj/provider/http/account/AccountDecoder.java
+++ operation-service/interfaceadapter/src/main/java/com/github/fabriciolfj/provider/http/account/AccountDecoder.java
@@ -1,14 +1,21 @@
 package com.github.fabriciolfj.provider.http.account;
 
+import com.github.fabriciolfj.domain.exceptions.AccountException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
-
-import javax.security.auth.login.AccountException;
+import org.springframework.http.HttpStatus;
 
 public class AccountDecoder implements ErrorDecoder {
 
+    private final ErrorDecoder delegate = new ErrorDecoder.Default();
+
     @Override
     public Exception decode(final String msg, final Response response) {
-        return new AccountException(msg + " , http status: " + response.status());
+
+        if (response.status() == HttpStatus.BAD_REQUEST.value()) {
+            throw new AccountException(msg + " , http status: " + response.status());
+        }
+
+        return delegate.decode(msg, response);
     }
 }
